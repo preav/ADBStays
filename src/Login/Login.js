74aa829f13067c81f27d572bd9a0aee86123e0af
Shivayan Bora <preetiyadav.kit@gmail.com>
2018-05-02 22:57:23 +0530

    Login screen update

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -5,11 +5,11 @@ class Login extends Component{
 	render(){
 		return (
 			<div className="loginMain">
-				<h3> Login to Continue </h3>
+				<h3 className="loginHeader"> Login to Continue </h3>
 				<input type="text" placeholder="Username or email" className="inputButton"/><br/>
 				<input type="password" placeholder="Password" className="inputButton"/><br/>
 				<input type="submit" className="inputButton submitButton"/>
-				<p> New user? <a href="#">Sign Up</a> here </p>
+				<p> New user? <a href="www.google.com">Sign Up</a> here </p>
 			</div>
 			)
 	}
